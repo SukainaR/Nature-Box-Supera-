32020972865621f4585469d9099549eabf24f3b6
VeeMR <124574340+VeeMR@users.noreply.github.com>
2023-03-16 12:56:55 +0000

    Update RaycastCamera.cs

--- Assets/Scripts/RaycastCamera.cs
+++ Assets/Scripts/RaycastCamera.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RaycastCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawRay(mousePos, Camera.main.transform.forward * 1000, Color.green);
+        }
     }
 
     // Update is called once per frame
